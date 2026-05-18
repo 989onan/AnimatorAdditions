f22ab310532172f9be2266e052bfd4b251ab4ccf
989onan <onanchew@gmail.com>
2026-05-18 04:27:48 -0400

    Trying to make a component member copier thingamabob

--- AnimatorAdditions/AnimatorAdditions.cs
+++ AnimatorAdditions/AnimatorAdditions.cs
@@ -1,13 +1,15 @@
-﻿using Elements.Assets;
-using Elements.Core;
+﻿using Elements.Core;
 using FrooxEngine;
 using FrooxEngine.UIX;
 using HarmonyLib;
 using ResoniteModLoader;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
-using FrooxEngine.Store;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace AnimatorAdditions
@@ -31,7 +33,161 @@ namespace AnimatorAdditions
             Harmony harmony = new Harmony($"{Author}.{Name}");
             Msg("patching method for animator ui");
             harmony.Patch(typeof(Animator).GetMethod(nameof(Animator.BuildInspectorUI)), postfix: new HarmonyMethod(AnimatorAdditionsPatch.PostFix));
+
+            //AccessTools.GetDeclaredMethods(typeof(SlotComponentReceiver))
+
+            //my terrible way of doing things - @989onan
+            /*
+            Type internal_type_async = null;
+            MethodInfo internal_sync_method = null;
+
+            MethodInfo MethodThing = typeof(SlotComponentReceiver).FirstMethod((i) => {
+                Msg(i.Name);
+
+
+                return i.GetMethodBody().LocalVariables.FirstOrDefault((j) =>
+                {
+
+                    Msg(j.LocalType);
+                    if(j.LocalType.ToString().Contains("DisplayClass1_1"))
+                    {
+                        internal_type_async = j.LocalType;
+                        return true;
+                    }
+                    return false;
+                }, null) != null;
+            });
+            if(internal_type_async != null) 
+            {
+                //FieldInfo hello = internal_type_async.GetAllNestedTypes().FirstOrDefault((i) => i.Name == "IAsyncStateMachine.MoveNext()");
+                foreach (MethodInfo item in ((Type)internal_type_async.GetMembers(BindingFlags.NonPublic)[0]).EnumerateAllInstanceMethods())
+                {
+                    if (item.Name == "MoveNext")
+                    {
+                        internal_sync_method = item;
+                    }
+                }
+            }*/
+            //99% credit to NepuShiro!
+            Type stateMachineType = typeof(SlotComponentReceiver).InnerTypes().FirstOrDefault(x => x.Name.StartsWith("<>") && x.InnerTypes().Any());
+
+            MethodInfo stolencodethingy = AccessTools.GetDeclaredMethods(stateMachineType).FirstOrDefault(x => x.Name == "<TryReceive>b__1");
+
+            
+            MethodInfo method = AccessTools.AsyncMoveNext(AccessTools.FirstMethod(stateMachineType, x => x.ReturnType == typeof(Task)));
+
+            harmony.Patch(stolencodethingy, transpiler: new HarmonyMethod(DynBonePatch.Transpiler2));
+
+            harmony.Patch(method, transpiler: new HarmonyMethod(DynBonePatch.Transpiler));
+            
+
             harmony.PatchAll();
+
+        }
+
+        //99% credit to NepuShiro!
+        public static class DynBonePatch
+        {
+            public static List<CodeInstruction> codes3 = new List<CodeInstruction>();
+
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                Msg("starting evaluation of code");
+                List <CodeInstruction> codes = instructions.ToList();
+                bool first_result = false;
+                int insert_point = -1;
+                for (int i = 0; i < instructions.Count(); i++)
+                {
+                    CodeInstruction code = codes[i];
+                    if (code.operand?.ToString().Contains("GetResult") == true)
+                    {
+                        first_result = true;
+                        continue;
+                    }
+                    if (first_result && code.opcode == OpCodes.Dup)
+                    {
+                        insert_point = i;
+                        break;
+                    }
+                }
+
+                Msg("Insert point is: " + insert_point.ToString());
+                Msg("Code is: " + codes[insert_point]);
+
+
+                List<CodeInstruction> codes2 =
+                [
+                    new CodeInstruction(OpCodes.Dup),
+                    .. codes3,
+                    new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(DynBonePatch), nameof(DynBonePatch.patch_context_menu)))
+                ];
+
+                codes.InsertRange(insert_point+1, codes2);
+
+                return codes.AsEnumerable();
+            }
+
+            public static IEnumerable<CodeInstruction> Transpiler2(IEnumerable<CodeInstruction> instructions)
+            {
+                codes3.AddRange(instructions.ToList().GetRange(0, 7));
+
+                return instructions;
+            }
+
+            public static void patch_context_menu(ContextMenu menu, Slot instance, FrooxEngine.Component component)
+            {
+                Msg("attach component 1");
+                Msg("component type is: " + component.GetType().Name);
+                if (component.GetType().IsAssignableTo(typeof(DynamicBoneChain)))
+                {
+                    DynamicBoneChain componentdyn = component as DynamicBoneChain;
+                    var newitem = menu.AddItem("Copy Dynamic Bone Linked", ((Uri)(null)), new colorX(1, 1, 1, 1));
+
+                    ((IButton)newitem).LocalPressed += (IButton b, ButtonEventData e) => {
+                        DynamicBoneChain newcomponent = instance.AttachComponent<DynamicBoneChain>();
+                        newcomponent.CollideWithBody.DriveFrom(componentdyn.CollideWithBody, true);
+                        newcomponent.CollideWithHead.DriveFrom(componentdyn.CollideWithHead, true);
+                        newcomponent.CollideWithLeftHand.DriveFrom(componentdyn.CollideWithLeftHand, true);
+                        newcomponent.CollideWithOwnBody.DriveFrom(componentdyn.CollideWithOwnBody, true);
+                        newcomponent.CollideWithRightHand.DriveFrom(componentdyn.CollideWithRightHand, true);
+                        newcomponent.Damping.DriveFrom(componentdyn.Damping, true);
+                        newcomponent.Stiffness.DriveFrom(componentdyn.Stiffness, true);
+                        newcomponent.Elasticity.DriveFrom(componentdyn.Elasticity, true);
+                        newcomponent.Inertia.DriveFrom(componentdyn.Inertia, true);
+                        newcomponent.InertiaForce.DriveFrom(componentdyn.InertiaForce, true);
+                        newcomponent.GrabPriority.DriveFrom(componentdyn.GrabPriority, true);
+                        newcomponent.GrabSlipping.DriveFrom(componentdyn.GrabSlipping, true);
+                        newcomponent.GrabRadiusTolerance.DriveFrom(componentdyn.GrabRadiusTolerance, true);
+                        newcomponent.BaseBoneRadius.DriveFrom(componentdyn.BaseBoneRadius, true);
+                        newcomponent.AllowSteal.DriveFrom(componentdyn.AllowSteal, true);
+                        newcomponent.ActiveUserRootOnly.DriveFrom(componentdyn.ActiveUserRootOnly, true);
+                        newcomponent.DynamicPlayerCollision.DriveFrom(componentdyn.DynamicPlayerCollision, true);
+                        newcomponent.Gravity.DriveFrom(componentdyn.Gravity, true);
+                        newcomponent.GravitySpace.Default.DriveFrom(componentdyn.GravitySpace.Default, true);
+                        newcomponent.GravitySpace.LocalSpace.DriveFrom(componentdyn.GravitySpace.LocalSpace, true);
+                        newcomponent.GravitySpace.OverrideRootSpace.DriveFrom(componentdyn.GravitySpace.OverrideRootSpace, true);
+                        newcomponent.GravitySpace.UseParentSpace.DriveFrom(componentdyn.GravitySpace.UseParentSpace, true);
+                        newcomponent.IgnoreOwnLeftHand.DriveFrom(componentdyn.IgnoreOwnLeftHand, true);
+                        newcomponent.IgnoreOwnRightHand.DriveFrom(componentdyn.IgnoreOwnRightHand, true);
+                        newcomponent.IgnoreGrabOnFirstBone.DriveFrom(componentdyn.IgnoreGrabOnFirstBone, true);
+                        newcomponent.IsGrabbable.DriveFrom(componentdyn.IsGrabbable, true);
+                        newcomponent.GrabVibration.DriveFrom(componentdyn.GrabVibration, true);
+                        newcomponent.GrabTerminalBones.DriveFrom(componentdyn.GrabTerminalBones, true);
+                        newcomponent.MaxStretchRatio.DriveFrom(componentdyn.MaxStretchRatio, true);
+                        newcomponent.LocalForce.DriveFrom(componentdyn.LocalForce, true);
+                        newcomponent.Stiffness.DriveFrom(componentdyn.Stiffness, true);
+                        newcomponent.StretchRestoreSpeed.DriveFrom(componentdyn.StretchRestoreSpeed, true);
+                        newcomponent.UseUserGravityDirection.DriveFrom(componentdyn.UseUserGravityDirection, true);
+                        newcomponent.UseLocalUserSpace.DriveFrom(componentdyn.UseLocalUserSpace, true);
+
+
+
+
+                    };
+                }
+                
+            }
+
 
         }
 
@@ -87,7 +243,7 @@ namespace AnimatorAdditions
                     __instance.RunSynchronously(() =>
                     {
                         Msg("creating new static provider");
-                        StaticAnimationProvider hello = (__instance.Clip.Target as Component).Slot.AttachComponent<StaticAnimationProvider>();
+                        StaticAnimationProvider hello = (__instance.Clip.Target as FrooxEngine.Component).Slot.AttachComponent<StaticAnimationProvider>();
                         Msg("setting URI");
                         hello.URL.Value = task.Result;
                         __instance.Clip.Target = hello;
